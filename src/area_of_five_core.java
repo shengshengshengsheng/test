5136ccc9a3ed5155f08e431e9730679f4db3e8ac
xuqiangsheng <xuqiangsheng@rfchina.com>
2019-08-13 16:19:50 +0800

    计算五角星的面积

---
+++ src/area_of_five_core.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/**
+ * 计算五角星的面积
+ * */
+    public class area_of_five_core {
+        public static void main(String[] args)
+        {
+            final double PI=3.1415926;
+            Scanner input=new Scanner(System.in);
+            System.out.println("请输入五边形中心到顶点的距离：");
+            double r=input.nextDouble();
+            double s=2*r*Math.sin(PI/5);
+            double area=5.0*Math.pow(s,2)/(4*Math.tan(PI/5));
+            System.out.println(area);
+        }
+}
