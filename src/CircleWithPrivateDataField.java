5f336658d795d573d7c37ce1f532787dc87b79bf
xuqiangsheng <xuqiangsheng@rfchina.com>
2019-08-14 09:29:17 +0800

    圆的私有类以及方法

---
+++ src/CircleWithPrivateDataField.java
@@ -0,0 +1,24 @@
+public class CircleWithPrivateDataField {
+    private double radious;
+    private double PI=3.14159265358;
+    CircleWithPrivateDataField()
+    {
+        this.radious=Math.random()*100;
+    }
+    CircleWithPrivateDataField(double radious)
+    {
+        this.radious=radious;
+    }
+    public void setRadious(double radious)
+    {
+        this.radious=radious;
+    }
+    public double getRadious()
+    {
+        return radious;
+    }
+    public double getArea()
+    {
+        return PI*radious*radious;
+    }
+}
